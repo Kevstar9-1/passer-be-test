167fbd8112870dd27ff0149383434520c7e54eb8
KevHernandez09 <kevstar924@gmail.com>
2025-07-21 18:39:56 -0600

    WIP: cambios para TS-07 en transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const transactionController = require('../controllers/transactions');
 
 router.post('/', transactionController.createTransaction);
-router.get('/:pk_transaction', transactionController.getTransaction);
+router.get('/:pk_transaction', transactionController.getTransaction);//Get por transaccion especifica por pk_transaction
 router.put('/:pk_transaction', transactionController.updateTransaction);
 module.exports = router;
