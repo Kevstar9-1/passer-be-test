64b16be38833bfe936de8c69186220e011953834
KevHernandez09 <kevstar924@gmail.com>
2025-07-21 18:54:54 -0600

    TS-07: update routes,controllers,services and models

--- models/transactions.js
+++ models/transactions.js
@@ -13,6 +13,35 @@ const createTransaction = (pk_transaction, fk_user, description, amount) => {
   }
 };
 
+const getTransaction = (pk_transaction) => {
+  try {
+    return postgresql.public.one(`
+      SELECT * FROM transaction
+      WHERE pk_transaction = ${pk_transaction}
+    `);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+const updateTransaction = (pk_transaction, fk_user, description, amount) => {
+  try {
+    return postgresql.public.one(`
+      UPDATE transaction
+      SET fk_user = ${fk_user},
+          description = '${description}',
+          amount = ${amount}
+      WHERE pk_transaction = ${pk_transaction}
+      RETURNING *;
+    `);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+
 module.exports = {
-  createTransaction
+  createTransaction,
+  getTransaction,
+  updateTransaction
 };
--- services/transactions.js
+++ services/transactions.js
@@ -4,6 +4,16 @@ const createTransaction = async (pk_transaction, fk_user, description, amount) =
   return await transactionModel.createTransaction(pk_transaction, fk_user, description, amount);
 };
 
+const getTransaction = async (pk_transaction) => {
+  return await transactionModel.getTransaction(pk_transaction);
+};
+
+const updateTransaction = async (pk_transaction, fk_user, description, amount) => {
+  return await transactionModel.updateTransaction(pk_transaction, fk_user, description, amount);
+};
+
 module.exports = {
-  createTransaction
+  createTransaction,
+  getTransaction,
+  updateTransaction
 };
